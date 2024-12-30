6dcddc0328dad8f384bf2741fe6f3c78cef30649
Suhana Sayyad <91276638+SuhanaSayyad@users.noreply.github.com>
2024-12-30 14:59:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     // Here you could add functionality to send the form data,
     // for example using fetch to send it to your server or email service.
     
-    alert('Thank you for your message!');
+    alert('Thank you for your message! I will get back to you soon.');
 });
