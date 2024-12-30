c8abee47a7e73ebe69de12e3ae5332cbc8f718b0
Suhana Sayyad <91276638+SuhanaSayyad@users.noreply.github.com>
2024-12-30 14:57:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Here you could add functionality to send the form data,
+    // for example using fetch to send it to your server or email service.
+    
+    alert('Thank you for your message!');
+});
